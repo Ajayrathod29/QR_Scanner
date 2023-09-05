08f941d7ede75979636550cbe09c905239f3703d
Ajay Rathod <ajayrathod1137@gmail.com>
2023-09-05 20:05:53 +0530

    initial commit

---
+++ app/src/main/java/com/example/qrscanerjetpack/MainActivity.kt
@@ -0,0 +1,59 @@
+package com.example.qrscanerjetpack
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.qrscanerjetpack.ui.theme.QRCodeScannerTheme
+import com.example.qrscanerjetpack.ui.theme.screens.QRCodeScannerScreen
+
+const val URL_KEY: String = "UrlKey"
+
+class MainActivity : ComponentActivity() {
+
+    private var urlText by mutableStateOf("")
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MainScreen(
+                urlText = urlText,
+                onUrlTextUpdate = {
+                    urlText = it
+                }
+            )
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        outState.putString(URL_KEY, urlText)
+        super.onSaveInstanceState(outState) // need to be called last
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        val restoreUrlText = savedInstanceState.getString(URL_KEY)
+
+        if(restoreUrlText != null) urlText = restoreUrlText
+    }
+}
+
+@Composable
+fun MainScreen(
+    useSystemUIController: Boolean = true,
+    urlText:String,
+    onUrlTextUpdate: (String) -> Unit) {
+
+    QRCodeScannerTheme(useSystemUIController = useSystemUIController) {
+        QRCodeScannerScreen(urlText, onUrlTextUpdate)
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    MainScreen(useSystemUIController = false, urlText= "", onUrlTextUpdate = {})
+}
